938d2cc47f08675e941a6e485e4604d5a488bd3b
Onetatcode <alikhanclues.@gmail.com>
2025-08-22 16:50:32 -0700

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import {Button} from '@/components/ui/button'; 
+
+export default function Home() {
+  return (
+    <div>
+      <Button className='bg-emerald-500 text-white hover:bg-emerald-600 px-4 py-2 rounded shadow  hover:shadow-lg transition duration-300 ease-in-out'>
+        Click Me
+      </Button>
+    </div>
+  );
+}
